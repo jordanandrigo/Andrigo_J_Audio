90d17379380b72480daf71b71f4f75911a822c7e
Jordan Andrigo <jordanandrigo@Jordans-MBP.fc.ca>
2020-02-14 10:43:22 -0500

    push

---
+++ js/main.js
@@ -0,0 +1,47 @@
+(() => {
+
+	//grab all the buttons and store them in a variable 
+	const playButtons = document.querySelectorAll('playTrack'),
+			pauseButtons = document.querySelectorAll('pauseTrack'),
+			rwButtons = document.querySelectorAll('rwTrack'),
+			audioElement = document.querySelector('#jukebox');
+
+
+
+
+
+	function playTrack() {
+		//debugger;
+
+		//hat audio track do we want? -> this is set on the bottom 
+		let audioSource = `audio/${this.dataset.trackref}.mp3`;
+
+		//set the src attribute for the audio element 
+		audioElement.src = audioSource;
+
+		//load the mp3 into the audio element, and play it
+		audioElement.load();
+
+		playTrack();
+	}
+
+	function playTrack() {
+		audioElement.play();
+	}
+
+	function pauseTrack() {
+		audioElement.pause();
+	}
+
+	function rwTrack() {
+		audioElement.currentTime = 0;
+	}
+
+
+
+
+
+	playButtons.forEach(button => button.addEventListener("click", loadTrack));
+	pauseButtons.forEach(button => button.addEventListener("click", pauseTrack));
+	rwButtons.forEach(button => button.addEventListener("click", rwTrack));
+})()
